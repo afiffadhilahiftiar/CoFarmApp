b94cbe73feb6f02b1fa8df48e941f25e9b9658e6
Afif Fadhilah Iftiar <apipvaxs@gmail.com>
2025-10-15 16:06:28 +0700

    Update ApiConfig.java

--- app/src/main/java/com/example/contractfarmingapp/ApiConfig.java
+++ app/src/main/java/com/example/contractfarmingapp/ApiConfig.java
@@ -2,6 +2,6 @@ package com.example.contractfarmingapp;
 
 public class ApiConfig {
     // Base URL aplikasi kamu
-    public static final String BASE_URL = "https://sistemcerdasindonesia.com/contractfarming/";
+    public static final String BASE_URL = "https://example.com/";
 }
 
