dc8660800f39c2ecaa3b5dc95a1694a300b05882
Afif Fadhilah Iftiar <apipvaxs@gmail.com>
2025-12-11 06:24:46 +0700

    Update ApiClient.java

--- app/src/main/java/com/example/contractfarmingapp/network/ApiClient.java
+++ app/src/main/java/com/example/contractfarmingapp/network/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "https://sistemcerdasindonesia.com//contractfarming/";
+    private static final String BASE_URL = "https://example.com//contractfarming/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
